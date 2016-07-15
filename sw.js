593276bbd7288dc42adac0ea7127ca867daf04f5
Antony D'Andrea <antony492@yahoo.co.uk>
2016-07-15 07:52:08 +0100

    now serves up cached page when offline

--- sw.js
+++ sw.js
@@ -1,11 +1,34 @@
 self.addEventListener('install', event => {
-
+    event.waitUntil(
+        caches.open('offline-cache').then(function(cache) {
+            return cache.addAll([
+               'css/main.css',
+               'index.php'
+            ]);
+        })
+    );
 });
+
 
 self.addEventListener('activate', event => {
 
 });
 
 self.addEventListener('fetch', event => {
-
+    // request.mode = navigate isn't supported in all browsers
+    // so include a check for Accept: text/html header.
+    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+            fetch(event.request.url).catch(error => {
+                // Return the offline page
+                return caches.match('index.php');
+            })
+        );
+    } else{
+        // Respond with everything else if we can
+        event.respondWith(caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+            })
+        );
+    }
 });
