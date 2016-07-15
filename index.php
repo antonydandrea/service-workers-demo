a53341121c9061a2ddbddf54e3a0f53bb6d97afe
Antony D'Andrea <antony492@yahoo.co.uk>
2016-07-15 07:07:13 +0100

    created my first service worker and successfully registered it to the page

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
         <link rel="stylesheet" type="text/css" href="css/main.css"/>
     </head>
     <body>
+        <script>
+            if ('serviceWorker' in navigator) {
+                navigator.serviceWorker.register('sw.js').then(function(registration) {
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                }).catch(function(err){
+                   console.log('ServiceWorker registration failed: ', err); 
+                });
+            }
+        </script>
         <h1>Simple ToDo App</h1>
         <div id="main-content">
             <form method="POST">
