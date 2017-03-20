32d9d4fa7f952a580de617b16fd8510022e8fa26
Antony D'Andrea <antony492@yahoo.co.uk>
2017-03-20 19:24:32 +0000

    remove console

--- sw.js
+++ sw.js
@@ -18,7 +18,6 @@ self.addEventListener('fetch', event => {
     // request.mode = navigate isn't supported in all browsers
     // so include a check for Accept: text/html header.
     if (event.request.method !== 'POST' && (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html')))) {
-        console.log('processing via SW');
         event.respondWith(
             fetch(event.request.url).catch(error => {
                 // Return the offline page
@@ -26,7 +25,6 @@ self.addEventListener('fetch', event => {
             })
         );
     } else{
-        console.log('passwing through to server');
         // Respond with everything else if we can
         event.respondWith(caches.match(event.request).then(function (response) {
             return response || fetch(event.request);
